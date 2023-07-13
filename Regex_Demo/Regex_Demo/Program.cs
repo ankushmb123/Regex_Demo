2db4587112d552c52baa73e69c4cf636de3e3421
Ankush Bahekar <ankushmb9@gmail.com>
2023-07-13 11:56:13 +0530

    [Ankush] Add:Regex Program.

---
+++ Regex_Demo/Regex_Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Regex_Demo
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            {
+                static void Main(string[] args)
+                {
+                    Regex_Display regexDisplay = new Regex_Display();
+                    regexDisplay.Display();
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
+
