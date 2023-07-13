0afe58229800c05431ac86d6d7f88ee105e4c480
Ankush Bahekar <ankushmb9@gmail.com>
2023-07-13 12:11:53 +0530

    [Ankush] Add:Checking if given lastname matches the pattern.

--- Regex_Demo/Regex_Demo/Program.cs
+++ Regex_Demo/Regex_Demo/Program.cs
@@ -17,6 +17,7 @@ namespace Regex_Demo
                     Regex_Display regexDisplay = new Regex_Display();
                     regexDisplay.Display();
                     regexDisplay.NameValidate();
+                    regexDisplay.Last_NameValidate();
                     Console.ReadLine();
 
                 }
